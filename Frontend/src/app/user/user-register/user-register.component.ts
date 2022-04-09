9e1a7d054fe0e81c9d453b1c2320cd3e64b5ac7e
sohail <emailtosohail7@gmail.com>
2022-04-09 16:57:59 +0530

    registration validation added

--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -13,12 +13,34 @@ export class UserRegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registrationForm = new FormGroup({
-      userName: new FormControl('Sohail', Validators.required),
+      userName: new FormControl('', Validators.required),
       email: new FormControl(null,[Validators.required , Validators.email]),
       password: new FormControl(null,[Validators.required , Validators.minLength(8)]),
       confirmPassword: new FormControl(null,[Validators.required , Validators.minLength(8)]),
       mobile: new  FormControl(null,[Validators.required , Validators.maxLength(10)])
-    });
+    }, this.passwordMatchingValidator);
+  }
+  passwordMatchingValidator(fg:FormGroup): Validators{
+    return fg.get('password').value === fg.get('confirmPassword').value ? null :
+    {notMatched:true}
+  }
+
+  //Getter methos for all form controls
+
+  get userName(){
+    return this.registrationForm.get('userName') as FormControl;
+  }
+  get email(){
+    return this.registrationForm.get('email') as FormControl;
+  }
+  get password(){
+    return this.registrationForm.get('password') as FormControl;
+  }
+  get confirmPassword(){
+    return this.registrationForm.get('confirmPassword') as FormControl;
+  }
+  get mobile(){
+    return this.registrationForm.get('mobile') as FormControl;
   }
   onSubmit(){
     console.log(this.registrationForm);
