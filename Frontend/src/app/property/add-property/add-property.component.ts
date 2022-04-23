f98730b2362965ed84f0680306c486db18b364c3
sohail <emailtosohail7@gmail.com>
2022-04-23 15:42:08 +0530

    minnor changes in add property

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
 import { IPropertyBase } from 'src/app/model/ipropertyBase';
@@ -7,46 +12,185 @@ import { IPropertyBase } from 'src/app/model/ipropertyBase';
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
-  styleUrls: ['./add-property.component.css']
+  styleUrls: ['./add-property.component.css'],
 })
 export class AddPropertyComponent implements OnInit {
   @ViewChild('formTabs') formTabs: TabsetComponent;
+  addPropertyForm: FormGroup;
+  nextClicked: boolean;
 
-  numberofRoom:Array<string>=['1','2','3','4'];
-  propertyType:Array<string>=['House','Apartment','Duplex'];
-  furnishType:Array<string>=['Fully','Semi','Unfurnished'];
-  mainEntrence:Array<string>=['East','West','South','North'];
+  propertyType: Array<string> = ['House', 'Apartment', 'Duplex'];
+  furnishType: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
+  mainEntrence: Array<string> = ['East', 'West', 'South', 'North'];
 
-  propertyView :IPropertyBase = {
-    Id:null,
-    Name:'',
-    Price:null,
-    SellRent:null,
-    PType:null,
-    FType:null,
-    BHK:null,
-    BuiltArea:null,
-    City:null,
-    RTM:null
-
+  propertyView: IPropertyBase = {
+    Id: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    PType: null,
+    FType: null,
+    BHK: null,
+    BuiltArea: null,
+    City: null,
+    RTM: null,
   };
-  addPropertyForm: any;
 
-  constructor(private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit() {
+    this.createAddPropertyForm();
   }
 
-  onBack(){
-      this.router.navigate(['/']);
+  createAddPropertyForm() {
+    this.addPropertyForm = this.fb.group({
+      BasicInfo: this.fb.group({
+        SellRent: ['1', Validators.required],
+        BHK: [null, Validators.required],
+        PType: [null, Validators.required],
+        FType: [null, Validators.required],
+        Name: [null, Validators.required],
+        City: [null, Validators.required],
+
+      }),
+      PriceInfo: this.fb.group({
+        Price: [null, Validators.required],
+        BuiltArea: [null, Validators.required],
+        Security: [null, Validators.required],
+        Maintenance: [null, Validators.required],
+        CarpetArea: [null, Validators.required]
+      }),
+    });
   }
-  onSubmit(){
+
+    // #region <Getter Methods>
+    // #region <FormGroups>
+    get BasicInfo() {
+        return this.addPropertyForm.controls['BasicInfo'] as FormGroup;
+    }
+
+    get PriceInfo() {
+        return this.addPropertyForm.controls['PriceInfo'] as FormGroup;
+    }
+
+    get AddressInfo() {
+        return this.addPropertyForm.controls['AddressInfo'] as FormGroup;
+    }
+
+    get OtherInfo() {
+        return this.addPropertyForm.controls['OtherInfo'] as FormGroup;
+    }
+    // #endregion
+
+    // #region <Form Controls>
+    get SellRent() {
+        return this.BasicInfo.controls['SellRent'] as FormControl;
+    }
+
+    get BHK() {
+        return this.BasicInfo.controls['BHK'] as FormControl;
+    }
+
+    get PType() {
+        return this.BasicInfo.controls['PType'] as FormControl;
+    }
+
+    get FType() {
+        return this.BasicInfo.controls['FType'] as FormControl;
+    }
+
+    get Name() {
+        return this.BasicInfo.controls['Name'] as FormControl;
+    }
+
+    get City() {
+        return this.BasicInfo.controls['City'] as FormControl;
+    }
+
+    get Price() {
+        return this.PriceInfo.controls['Price'] as FormControl;
+    }
+
+    get BuiltArea() {
+        return this.PriceInfo.controls['BuiltArea'] as FormControl;
+    }
+
+    get CarpetArea() {
+        return this.PriceInfo.controls['CarpetArea'] as FormControl;
+    }
+
+    get Security() {
+        return this.PriceInfo.controls['Security'] as FormControl;
+    }
+
+    get Maintenance() {
+        return this.PriceInfo.controls['Maintenance'] as FormControl;
+    }
+
+    get FloorNo() {
+        return this.AddressInfo.controls['FloorNo'] as FormControl;
+    }
+
+    get TotalFloor() {
+        return this.AddressInfo.controls['TotalFloor'] as FormControl;
+    }
+
+    get Address() {
+        return this.AddressInfo.controls['Address'] as FormControl;
+    }
+
+    get LandMark() {
+        return this.AddressInfo.controls['LandMark'] as FormControl;
+    }
+
+    get RTM() {
+        return this.OtherInfo.controls['RTM'] as FormControl;
+    }
+
+    get PossessionOn() {
+        return this.OtherInfo.controls['PossessionOn'] as FormControl;
+    }
+
+    get AOP() {
+        return this.OtherInfo.controls['AOP'] as FormControl;
+    }
+
+    get Gated() {
+        return this.OtherInfo.controls['Gated'] as FormControl;
+    }
+
+    get MainEntrance() {
+        return this.OtherInfo.controls['MainEntrance'] as FormControl;
+    }
+
+    get Description() {
+        return this.OtherInfo.controls['Description'] as FormControl;
+    }
+
+    // #endregion
+    // #endregion
+
+  onBack() {
+    this.router.navigate(['/']);
+  }
+  onSubmit() {
+    this.nextClicked=true;
+    if (this.BasicInfo.invalid){
+      this.formTabs.tabs[0].active = true;
+      return;
+    }
+    if (this.PriceInfo.invalid){
+      this.formTabs.tabs[1].active = true;
+      return;
+    }
     console.log('Congrats your form submitted.');
+    console.log('SellRent=' + this.addPropertyForm.value.BasicInfo.SellRent);
     console.log(this.addPropertyForm);
   }
-  selectTab(tabId: number) {
-
-      this.formTabs.tabs[tabId].active = true;
-
-  }
+  selectTab(NextTabId: number, IsCurrentTabValid: boolean) {
+    this.nextClicked = true;
+    if (IsCurrentTabValid) {
+        this.formTabs.tabs[NextTabId].active = true;
+    }
+}
 }
