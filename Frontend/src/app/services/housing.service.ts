d80c2b70526d5f52ce6fbab039f3a3c92e3b22dc
sohail <emailtosohail7@gmail.com>
2022-04-24 15:51:07 +0530

    multiple properties added

--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -14,6 +14,19 @@ export class HousingService {
     return this.http.get('data/properties.json').pipe(
       map((data) => {
         const propertiesArray: Array<IPropertyBase> = [];
+        const localProperties = JSON.parse(localStorage.getItem('newProp'));
+
+        if (localProperties) {
+          for (const id in localProperties) {
+            if (
+              localProperties.hasOwnProperty(id) &&
+              localProperties[id].SellRent === SellRent
+            ) {
+              propertiesArray.push(localProperties[id]);
+            }
+          }
+        }
+
         for (const id in data) {
           if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
             propertiesArray.push(data[id]);
@@ -24,6 +37,20 @@ export class HousingService {
     );
   }
   addProperty(property: Property) {
-    localStorage.setItem('newProp', JSON.stringify(property));
+    let newProp = [property];
+
+    //Add new property in array if newProp alredy exist in localStorage
+    if (localStorage.getItem('newProp')) {
+      newProp = [property, ...JSON.parse(localStorage.getItem('newProp'))];
+    }
+    localStorage.setItem('newProp', JSON.stringify(newProp));
+  }
+  newPropID() {
+    if (localStorage.getItem('PID')) {
+      return +localStorage.getItem('PID') + 1;
+    } else {
+      localStorage.setItem('PID', '101');
+      return 101;
+    }
   }
 }
