52ff2ec88be7b4d42f7309212b0a25f73ca8b4f4
sohail <emailtosohail7@gmail.com>
2022-04-24 15:06:58 +0530

    save to local storage

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -8,6 +8,9 @@ import {
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
 import { IPropertyBase } from 'src/app/model/ipropertyBase';
+import { Property } from 'src/app/model/property';
+import { AltertyfyService } from 'src/app/services/altertyfy.service';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-add-property',
@@ -18,6 +21,7 @@ export class AddPropertyComponent implements OnInit {
   @ViewChild('formTabs') formTabs: TabsetComponent;
   addPropertyForm: FormGroup;
   nextClicked: boolean;
+  property = new Property();
 
   propertyType: Array<string> = ['House', 'Apartment', 'Duplex'];
   furnishType: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
@@ -36,7 +40,13 @@ export class AddPropertyComponent implements OnInit {
     RTM: null,
   };
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private alertify: AltertyfyService,
+    private housingService:HousingService
+  ) {}
 
   ngOnInit() {
     this.createAddPropertyForm();
@@ -51,146 +61,202 @@ export class AddPropertyComponent implements OnInit {
         FType: [null, Validators.required],
         Name: [null, Validators.required],
         City: [null, Validators.required],
-
       }),
       PriceInfo: this.fb.group({
         Price: [null, Validators.required],
         BuiltArea: [null, Validators.required],
         Security: [null, Validators.required],
         Maintenance: [null, Validators.required],
-        CarpetArea: [null, Validators.required]
+        CarpetArea: [null, Validators.required],
+      }),
+      AddressInfo: this.fb.group({
+        FloorNo: [null],
+        TotalFloor: [null],
+        Address: [null, Validators.required],
+        LandMark: [null],
+      }),
+
+      OtherInfo: this.fb.group({
+        RTM: [null, Validators.required],
+        PossessionOn: [null, Validators.required],
+        AOP: [null],
+        Gated: [null],
+        MainEntrance: [null],
+        Description: [null],
       }),
     });
   }
 
-    // #region <Getter Methods>
-    // #region <FormGroups>
-    get BasicInfo() {
-        return this.addPropertyForm.controls['BasicInfo'] as FormGroup;
-    }
+  // #region <Getter Methods>
+  // #region <FormGroups>
+  get BasicInfo() {
+    return this.addPropertyForm.controls['BasicInfo'] as FormGroup;
+  }
 
-    get PriceInfo() {
-        return this.addPropertyForm.controls['PriceInfo'] as FormGroup;
-    }
+  get PriceInfo() {
+    return this.addPropertyForm.controls['PriceInfo'] as FormGroup;
+  }
 
-    get AddressInfo() {
-        return this.addPropertyForm.controls['AddressInfo'] as FormGroup;
-    }
+  get AddressInfo() {
+    return this.addPropertyForm.controls['AddressInfo'] as FormGroup;
+  }
 
-    get OtherInfo() {
-        return this.addPropertyForm.controls['OtherInfo'] as FormGroup;
-    }
-    // #endregion
+  get OtherInfo() {
+    return this.addPropertyForm.controls['OtherInfo'] as FormGroup;
+  }
+  // #endregion
 
-    // #region <Form Controls>
-    get SellRent() {
-        return this.BasicInfo.controls['SellRent'] as FormControl;
-    }
+  // #region <Form Controls>
+  get SellRent() {
+    return this.BasicInfo.controls['SellRent'] as FormControl;
+  }
 
-    get BHK() {
-        return this.BasicInfo.controls['BHK'] as FormControl;
-    }
+  get BHK() {
+    return this.BasicInfo.controls['BHK'] as FormControl;
+  }
 
-    get PType() {
-        return this.BasicInfo.controls['PType'] as FormControl;
-    }
+  get PType() {
+    return this.BasicInfo.controls['PType'] as FormControl;
+  }
 
-    get FType() {
-        return this.BasicInfo.controls['FType'] as FormControl;
-    }
+  get FType() {
+    return this.BasicInfo.controls['FType'] as FormControl;
+  }
 
-    get Name() {
-        return this.BasicInfo.controls['Name'] as FormControl;
-    }
+  get Name() {
+    return this.BasicInfo.controls['Name'] as FormControl;
+  }
 
-    get City() {
-        return this.BasicInfo.controls['City'] as FormControl;
-    }
+  get City() {
+    return this.BasicInfo.controls['City'] as FormControl;
+  }
 
-    get Price() {
-        return this.PriceInfo.controls['Price'] as FormControl;
-    }
+  get Price() {
+    return this.PriceInfo.controls['Price'] as FormControl;
+  }
 
-    get BuiltArea() {
-        return this.PriceInfo.controls['BuiltArea'] as FormControl;
-    }
+  get BuiltArea() {
+    return this.PriceInfo.controls['BuiltArea'] as FormControl;
+  }
 
-    get CarpetArea() {
-        return this.PriceInfo.controls['CarpetArea'] as FormControl;
-    }
+  get CarpetArea() {
+    return this.PriceInfo.controls['CarpetArea'] as FormControl;
+  }
 
-    get Security() {
-        return this.PriceInfo.controls['Security'] as FormControl;
-    }
+  get Security() {
+    return this.PriceInfo.controls['Security'] as FormControl;
+  }
 
-    get Maintenance() {
-        return this.PriceInfo.controls['Maintenance'] as FormControl;
-    }
+  get Maintenance() {
+    return this.PriceInfo.controls['Maintenance'] as FormControl;
+  }
 
-    get FloorNo() {
-        return this.AddressInfo.controls['FloorNo'] as FormControl;
-    }
+  get FloorNo() {
+    return this.AddressInfo.controls['FloorNo'] as FormControl;
+  }
 
-    get TotalFloor() {
-        return this.AddressInfo.controls['TotalFloor'] as FormControl;
-    }
+  get TotalFloor() {
+    return this.AddressInfo.controls['TotalFloor'] as FormControl;
+  }
 
-    get Address() {
-        return this.AddressInfo.controls['Address'] as FormControl;
-    }
+  get Address() {
+    return this.AddressInfo.controls['Address'] as FormControl;
+  }
 
-    get LandMark() {
-        return this.AddressInfo.controls['LandMark'] as FormControl;
-    }
+  get LandMark() {
+    return this.AddressInfo.controls['LandMark'] as FormControl;
+  }
 
-    get RTM() {
-        return this.OtherInfo.controls['RTM'] as FormControl;
-    }
+  get RTM() {
+    return this.OtherInfo.controls['RTM'] as FormControl;
+  }
 
-    get PossessionOn() {
-        return this.OtherInfo.controls['PossessionOn'] as FormControl;
-    }
+  get PossessionOn() {
+    return this.OtherInfo.controls['PossessionOn'] as FormControl;
+  }
 
-    get AOP() {
-        return this.OtherInfo.controls['AOP'] as FormControl;
-    }
+  get AOP() {
+    return this.OtherInfo.controls['AOP'] as FormControl;
+  }
 
-    get Gated() {
-        return this.OtherInfo.controls['Gated'] as FormControl;
-    }
+  get Gated() {
+    return this.OtherInfo.controls['Gated'] as FormControl;
+  }
 
-    get MainEntrance() {
-        return this.OtherInfo.controls['MainEntrance'] as FormControl;
-    }
+  get MainEntrance() {
+    return this.OtherInfo.controls['MainEntrance'] as FormControl;
+  }
 
-    get Description() {
-        return this.OtherInfo.controls['Description'] as FormControl;
-    }
+  get Description() {
+    return this.OtherInfo.controls['Description'] as FormControl;
+  }
 
-    // #endregion
-    // #endregion
+  // #endregion
+  // #endregion
 
   onBack() {
     this.router.navigate(['/']);
   }
   onSubmit() {
-    this.nextClicked=true;
-    if (this.BasicInfo.invalid){
+    this.nextClicked = true;
+    if (this.allTabsValid()) {
+      this.mapProperty();
+      this.housingService.addProperty(this.property)
+      this.alertify.success('Congrats your form submitted.');
+      console.log(this.addPropertyForm);
+    } else {
+      this.alertify.error('Please review the form and provide all valid');
+    }
+  }
+
+  mapProperty(): void {
+    this.property.SellRent = +this.SellRent.value;
+    this.property.BHK = this.BHK.value;
+    this.property.PType = this.PType.value;
+    this.property.Name = this.Name.value;
+    this.property.City = this.City.value;
+    this.property.FType = this.FType.value;
+    this.property.Price = this.Price.value;
+    this.property.Security = this.Security.value;
+    this.property.Maintenance = this.Maintenance.value;
+    this.property.BuiltArea = this.BuiltArea.value;
+    this.property.CarpetArea = this.CarpetArea.value;
+    this.property.FloorNo = this.FloorNo.value;
+    this.property.TotalFloors = this.TotalFloor.value;
+    this.property.Address = this.Address.value;
+    this.property.Address2 = this.LandMark.value;
+    this.property.RTM = this.RTM.value;
+    this.property.AOP = this.AOP.value;
+    this.property.Gated = this.Gated.value;
+    this.property.MainEntrance = this.MainEntrance.value;
+    this.property.Possession = this.PossessionOn.value;
+    this.property.Description = this.Description.value;
+    this.property.PostedOn = new Date().toString();
+  }
+
+  allTabsValid(): boolean {
+    if (this.BasicInfo.invalid) {
       this.formTabs.tabs[0].active = true;
-      return;
+      return false;
     }
-    if (this.PriceInfo.invalid){
+    if (this.PriceInfo.invalid) {
       this.formTabs.tabs[1].active = true;
-      return;
+      return false;
     }
-    console.log('Congrats your form submitted.');
-    console.log('SellRent=' + this.addPropertyForm.value.BasicInfo.SellRent);
-    console.log(this.addPropertyForm);
+    if (this.AddressInfo.invalid) {
+      this.formTabs.tabs[2].active = true;
+      return false;
+    }
+    if (this.OtherInfo.invalid) {
+      this.formTabs.tabs[3].active = true;
+      return false;
+    }
+    return true;
   }
   selectTab(NextTabId: number, IsCurrentTabValid: boolean) {
     this.nextClicked = true;
     if (IsCurrentTabValid) {
-        this.formTabs.tabs[NextTabId].active = true;
+      this.formTabs.tabs[NextTabId].active = true;
     }
-}
+  }
 }
--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -3,29 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { IPropertyBase } from '../model/ipropertyBase';
+import { Property } from '../model/property';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HousingService {
-
-constructor(private http:HttpClient) { }
-
-  // getAllProperties() {
-  //   return this.http.get('data/properties.json');
-  // }
-
-getAllProperties(SellRent: number) : Observable<IPropertyBase[]> {
-  return this.http.get('data/properties.json').pipe(
-    map(data=>{
-      const propertiesArray: Array<IPropertyBase> = [];
-      for (const id in data){
-        if(data.hasOwnProperty(id) && data[id].SellRent === SellRent){
-          propertiesArray.push(data[id]);
+  constructor(private http: HttpClient) {}
+  getAllProperties(SellRent: number): Observable<IPropertyBase[]> {
+    return this.http.get('data/properties.json').pipe(
+      map((data) => {
+        const propertiesArray: Array<IPropertyBase> = [];
+        for (const id in data) {
+          if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
+            propertiesArray.push(data[id]);
+          }
         }
-      }
-      return propertiesArray;
-    })
-  );
-}
+        return propertiesArray;
+      })
+    );
+  }
+  addProperty(property: Property) {
+    localStorage.setItem('newProp', JSON.stringify(property));
+  }
 }
