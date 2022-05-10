9d353b09c8195c49981dd88be7bc52f6b00eece0
sohail <emailtosohail7@gmail.com>
2022-05-10 11:19:43 +0530

    Post and delete method added

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -26,12 +26,32 @@ namespace WebAPI.Controllers
             var cities = await dc.Cities.ToListAsync();
             return Ok(cities);
         }
+        //Post the data in String format
          [HttpPost("add")]
         public async Task<IActionResult> AddCities(String cityName)
         {
             City city = new City();
             city.Name=cityName;
             await dc.Cities.AddAsync(city);
+            await dc.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        //Post the data in Json format
+         [HttpPost("post")]
+        public async Task<IActionResult> AddCities(City city)
+        {
+           
+            await dc.Cities.AddAsync(city);
+            await dc.SaveChangesAsync();
+            return Ok(city);
+        }
+        //Delete record
+         [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCities(int id)
+        {
+            var city = await dc.Cities.FindAsync(id);
+            dc.Cities.Remove(city);
             await dc.SaveChangesAsync();
             return Ok(city);
         }
